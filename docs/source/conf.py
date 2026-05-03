338e3adc3f59c5b2406cb97d266317d39c4c128f
famatf <84229080+famatf@users.noreply.github.com>
2026-05-04 00:05:45 +0800

    Update conf.py
    change html title

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,11 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'famaweb'
+project = 'Situs Interretialis Famae'
 copyright = '2026, famatf'
 author = 'famatf'
 release = ''
-html_title = "fama的网站"
+html_title = 'Situs Interretialis Famae'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
